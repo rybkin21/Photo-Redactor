67a21816425c8fead80726223de3087667b1e330
Ivan Rybkin <i.rybkin@icloud.com>
2025-03-03 14:12:31 +0300

    Initial Commit

---
+++ Photo Redactor/Photo_RedactorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_RedactorApp.swift
+//  Photo Redactor
+//
+//  Created by Ivan Rybkin on 03.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Photo_RedactorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
